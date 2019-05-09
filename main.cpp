20b5a321915a01ab9604bb1ffe7e51047c34f58a
hiscloud <49000237+hiscloud@users.noreply.github.com>
2019-05-08 19:58:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,12 +26,14 @@ class piece{
 	int col;
 	int row;
 	string name;
+	int ID;
 	
 	void init(string n){
 		srand( (unsigned)time( NULL ) );
 		col=rand()%5;
 		row=rand()%5;
 		name=n;
+		carry =false;
 	}
 	
 	
@@ -54,21 +56,44 @@ class table{
 			}	
 		}
 		
+		piece marvin,bunny,tweety,devil,c1,c2,mount;
+		bunny.init("B   ");
+		tweety.init("T   ");
+		c1.init("C   ");
+		c1.carrot=true;
+		c2.init("C   ");
+		c2.carrot=true;
+		devil.init("D   ");
+		marvin.init("M   ");
+		marvin.mv=true;
+		mount.init("F   ");
+		mount.mt=true;
+		addPiece(devil);	
+		addPiece(bunny);
+		addPiece(tweety);
+		addPiece(marvin);
+		
+		addPiece(mount);
+		addPiece(c1);
+		addPiece(c2);
+		
 			
 	}
 
 	void addPiece(piece p){  
-
-	
 		while(checkOn(p.col,p.row)){
 			p.col=rand()%5;
 			p.row=rand()%5;	
 				
 		}
 		matrix[p.col][p.row]=p.name;
+		
 		arr[arrIND]=p;
+		p.ID=arrIND;
+	
 		arrIND++;
-		p.carry=false;
+		//p.carry=false;
+		
 	}	
 	
 	void printTable(){
@@ -79,39 +104,58 @@ class table{
 		cout<<endl;
 		}
 	}	
+	
+	
+	
+	
 	//random a point which is around the piece
-	void move(piece p){
+	void move(int ind){
+		piece p=arr[ind];
+		
+		//cout<<p.name<<p.col<<p.row<<endl;/////////////////////////////////
 		matrix[p.col][p.row]="-   ";
-		int x=rand()%5,y=rand()%5;//(x,y)
-		while((checkOn(x,y))||(p.col-x>1||p.col-x<-1)||(p.row-y>1||p.row-y<-1)){//(then it should change)
+		int x=rand()%5,y=rand()%5;//(x,y) next place to step
+		while((checkOn(x,y)&&!getPiece(x,y).carrot)||(p.col-x>1||p.col-x<-1)||(p.row-y>1||p.row-y<-1)){//(then it should change)
 			x=rand()%5;
 			y=rand()%5;	
 		}
 		p.col=x;
 		p.row=y;
-		if(getPiece(x,y).carrot){
-			p.name-="   ";
-			p.name+="(c)";
-			
+		//cout<<x<<y<<endl;/////////////////////////////
+		if(getPiece(x,y).carrot){//carrot condition
+			if (p.ID==0)
+				p.name="D(C)";	
+			if(p.ID==1)
+				p.name="B(C)";
+			if(p.ID ==2)
+				p.name="T(C)";
+			if(p.ID==3)
+				p.name="M(C)";
+			p.carry=true;
+		}
+		if(getPiece(x,y).mt&&p.carrot){ //winning condition
+			cout<< p.name<<" wins"<<endl;	
 		}
 		matrix[p.col][p.row]=p.name;
-		setPiece();
-		
+		arr[ind].col=p.col;
+		arr[ind].row=p.row;
+		//setPiece();
 	}
+	
 	//check if the place has a piece
 	bool checkOn(int x1, int y1){
 		
 		for(int i=0;i<arrIND;i++){
-			if(x1==arr[i].col &&y1==arr[i].row)
+			if(x1==arr[i].col &&y1==arr[i].row){
 				return true;
+			}
 		}
 		return false;
 	}
 	
-	void setPiece(){
+	void setPiece(){/////////////////////////////////////////////////////////////////
 		for(int i=0;i<arrIND;i++){
 			matrix[arr[i].col][arr[i].row]=arr[i].name;
-			
 		}
 		
 	}
@@ -131,7 +175,8 @@ class CParallel
 {      public:
 		CParallel(){};
 		~CParallel(){};
-	
+		table *t;
+		
 		int thread_id_obj;
 		pthread_t thread_id;
 		pthread_mutex_t *toon_signal_mutex = NULL;
@@ -139,13 +184,15 @@ class CParallel
 		int stop;
 
 		void init(int value, MUTEX_SHARED *temp_SHARED,
-            pthread_mutex_t *temp_toon_signal_mutex){
+            pthread_mutex_t *temp_toon_signal_mutex,table *T){
 		    thread_id_obj = value;
 		    SHARED = (MUTEX_SHARED*)temp_SHARED;
 		    toon_signal_mutex = (pthread_mutex_t*)temp_toon_signal_mutex;
 		    stop = 0;
+			t=T;
 			
-			/////booleans not setted
+			
+			
 		}
 
 		static void *parallel_API(void *context)
@@ -159,7 +206,7 @@ class CParallel
             while(!stop){
                 toon_signal();
                 cout << "thread_id_obj = " << thread_id_obj << endl;
-                Sleep(1500);
+                
             }
             pthread_exit(NULL);
 			return 0;
@@ -175,6 +222,7 @@ class CParallel
                     cout << "SHARED->COUNTING = " << SHARED->COUNTING
                             << " SHARED->AGENT = " << SHARED->AGENT << endl;
                     SHARED->COUNTING++;
+					t->move(0);
                     if(SHARED->COUNTING==2){
                         stop=1;
                     }
@@ -184,6 +232,7 @@ class CParallel
                     cout << "SHARED->COUNTING = " << SHARED->COUNTING
                             << " SHARED->AGENT = " << SHARED->AGENT << endl;
                     SHARED->COUNTING++;
+					t->move(1);
                     if(SHARED->COUNTING==4){
                         stop=1;
                     }
@@ -194,6 +243,7 @@ class CParallel
                     cout << "SHARED->COUNTING = " << SHARED->COUNTING
                             << " SHARED->AGENT = " << SHARED->AGENT << endl;
                     SHARED->COUNTING++;
+					t->move(2);
                     if(SHARED->COUNTING==6){
                         stop=1;
                     }
@@ -201,6 +251,7 @@ class CParallel
 					
 					 // RUN 4th THREAD
                     SHARED->AGENT = thread_id_obj;
+					t->move(3);
                     cout << "SHARED->COUNTING = " << SHARED->COUNTING
                             << " SHARED->AGENT = " << SHARED->AGENT << endl;
                     SHARED->COUNTING++;
@@ -233,49 +284,15 @@ int total_threads = 5;
     MUTEX_SHARED SHARED;
     SHARED.COUNTING=0;
 
-	string table1[5][5];
-	for (int i=0;i<5;i++){
-		for (int j=0;j<5;j++){
-			table1[i][j]="-   ";
-		
-		}	
-		
-		
-	}
 /////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-piece marvin,bunny,tweety,devil,c1,c2,mount;
-bunny.init("B   ");
-tweety.init("T   ");
-c1.init("C   ");
-c1.carrot=true;
-c2.init("C   ");
-c2.carrot=true;
-devil.init("D   ");
-marvin.init("M   ");
-marvin.mv=true;
-mount.init("F   ");
-mount.mt=true;
-
-	table* myTable= new table();
-	myTable->init();
-	myTable->addPiece(devil);	
-	myTable->addPiece(bunny);
-	myTable->addPiece(tweety);
-	myTable->addPiece(marvin);
-	myTable->addPiece(mount);
-	myTable->addPiece(c1);
-	myTable->addPiece(c2);
+	table *myTable=new table();
+	myTable->init();	
 	myTable->printTable();
-	
-	myTable->move(devil);
-	myTable->move(bunny);// duplicated
-	
-	myTable->printTable();
+	myTable->move(0);
+	myTable->move(1);// duplicated
+	myTable->move(2);
+	myTable->move(3);
 	
 	
 	
@@ -286,18 +303,19 @@ mount.mt=true;
     pthread_mutex_init(&toon_signal_mutex, NULL);
 
     for(int i=0;i<total_threads;i++){
-        obj[i].init(i,&SHARED,&toon_signal_mutex);
+        obj[i].init(i,&SHARED,&toon_signal_mutex,myTable);
         pthread_create(&obj[i].thread_id, NULL,
                        &CParallel::parallel_API, &obj[i]);
-        //sleep(1);  // for linux/unix/mac
+        
         Sleep(1); // for windows
         pthread_join(obj[i].thread_id, NULL);
+		myTable->printTable();
     }
 
     pthread_mutex_destroy(&toon_signal_mutex);
 	
-    Sleep(800);
+    
     cout << "program finished!"<< endl;
-
+myTable->printTable();
     return 0;
 }
